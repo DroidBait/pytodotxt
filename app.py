eaccf29ec2355fe09683da0f81254100a1a8f9e1
Stephen <dickasonstephen@gmail.com>
2024-05-18 18:09:32 +0100

    Fix duplicated todos when filtering

--- app.py
+++ app.py
@@ -9,6 +9,11 @@ folder = config.get("General", "file_loc")
 
 def print_help():
     print("Python Todo.txt Help")
+    print("args:")
+    print("    ls or list: List all todos in todo.txt file")
+    print("        Example: app.py ls")
+    print("        Filters can also be applied. Add a space between each filter")
+    print("        Example: app.py ls @house +projectA")
     sys.exit()
 
 def get_action(args):
@@ -24,7 +29,6 @@ def get_list_of_lines_in_file(folder_loc):
     list_of_lines = []
     with open(file_loc) as f:
         for line in f:
-            #line = f.readline()
             list_of_lines.append(line)
     return list_of_lines
 
@@ -38,6 +42,22 @@ def print_todos(todos):
         print(todo, end="")
     return
 
+def are_filters_applied(args):
+    if len(args) > 2:
+        return True
+    else:
+        return False
+
+def filter_todos(args, todos):
+    filter_keys = args[2:]
+    filtered_todos = []
+    for filter in filter_keys:
+        for todo in todos:
+            if filter in todo:
+                if todo not in filtered_todos:
+                    filtered_todos.append(todo)
+    return filtered_todos
+
 
 if __name__ == "__main__":
     action = get_action(sys.argv)
@@ -45,7 +65,11 @@ if __name__ == "__main__":
         print_help()
     elif action == "ls" or action == "list":
         todos = get_list_of_lines_in_file(folder)
-        print_todos(todos=todos)
+        if are_filters_applied(sys.argv):
+            filtered_todos = filter_todos(args=sys.argv, todos=todos)
+            print_todos(filtered_todos)
+        else:
+            print_todos(todos=todos)
     else:
         print(action)
 
